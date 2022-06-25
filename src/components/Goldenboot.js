79d4c18b56bf25aae55da33c3838f2781620431a
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-06-25 22:07:47 +0530

    .

--- src/components/Goldenboot.js
+++ src/components/Goldenboot.js
@@ -1,35 +1,48 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import loadingimg from '../assets/loading.gif'
+
 
 const Goldenboot = () => {
 
     const [goalsScorers, setGoalScorers] = useState([])
+    const [loading, setLoading] = useState(true)
     const fetchGoals = async () => {
         const response = await axios.get('https://enfuego.herokuapp.com/goalscorers')
         console.log(response.data);
         setGoalScorers(response.data)
+        setLoading(false)
     }
     useEffect(() => {
         fetchGoals()
     }, [])
 
     return (
-        <div className='overflow-y-scroll'>
-            <h3 className='text-center text-2xl font-bold mb-6'>Goal Scorers</h3>
-            <div className='grid grid-cols-6 text-xl font-bold border-b-2 mx-4 mb-8 pb-4 border-black'>
-                <div className="col-span-3">Name</div>
-                <div className="col-span-2">Team</div>
-                <div className="col-span-1">Goals</div>
 
-            </div>
-            {goalsScorers.map((data) => (
-                <div className='grid grid-cols-6 mt-4 pb-4 border-b-2 mx-4 pb-4'>
-                    <div className="col-span-3"> {data.name} </div>
-                    <div className="col-span-2 "> {data.team_id}</div>
-                    <div className="col-span-1 text-center">{data.goalsscored}</div>
+        <div className='overflow-y-scroll'>
+            {loading ? <>
+                <div className="flex justify-center h-screen w-screen bg-white">
+                    <img src={loadingimg} alt="" className='flex items-center justify-center w-[20rem]' />
 
                 </div>
-            ))}
+            </> : <div className='bg-yellow'>
+                <h3 className='text-center text-2xl font-bold mb-6'>Goal Scorers</h3>
+                <div className='grid grid-cols-6 text-xl font-bold border-b-2 mx-4 mb-8 pb-4 border-black'>
+                    <div className="col-span-3">Name</div>
+                    <div className="col-span-2">Team</div>
+                    <div className="col-span-1">Goals</div>
+
+                </div>
+                {goalsScorers.map((data) => (
+                    <div className='grid grid-cols-6 mt-4 pb-4 border-b-2 mx-4 pb-4'>
+                        <div className="col-span-3"> {data.name} </div>
+                        <div className="col-span-2 "> {data.team_id}</div>
+                        <div className="col-span-1 text-center">{data.goalsscored}</div>
+
+                    </div>
+                ))}
+            </div>}
+
         </div>
 
     )
--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,23 +1,35 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import loadingimg from '../assets/loading.gif'
 
 const Rules = () => {
   const [rules, setRules] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     axios.get('https://enfuego.herokuapp.com/guidelines').then((res) => {
       setRules(res.data)
       console.log(res.data)
+      setLoading(false)
     }).catch((err) => console.log(err))
   }, [])
   return (
     <div>
+    {loading?<>
+      <div className="flex justify-center">
+                    <img src={loadingimg} alt="" className='flex items-center justify-center w-[20rem]' />
+
+                </div>
+    </>:<>
     <h3 className='text-center text-2xl font-bold mb-6'>Guidelines</h3>
-      {rules.map((data,key)=>(
-        <div key={key} className="mx-4 text-xl mt-2">
-          <li> {data.rule} </li>
-        </div>
-      ))}
+     
+     
+     {rules.map((data,key)=>(
+       <div key={key} className="mx-4 text-xl mt-2">
+         <li> {data.rule} </li>
+       </div>
+     ))}
+    </>}
     </div>
   )
 }
