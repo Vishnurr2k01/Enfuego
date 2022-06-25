f7b22778046acd448736cd9a9e7820166253d6d5
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-06-25 22:14:50 +0530

    .

--- src/components/Pointtable.js
+++ src/components/Pointtable.js
@@ -8,41 +8,44 @@ const Pointtable = () => {
     const [loading, setLoading] = useState(true)
     const [team, setTeam] = useState([])
 
-    // const fetchTable = async () => {
+    const fetchTable = async () => {
 
-    //     const response = await axios.get('https://enfuego.herokuapp.com/pointtable')
-    //     setTable(response.data)
-    // }
-    // useEffect(() => {
-    //     fetchTable()
-    // }, [table])
-    axios.get('https://enfuego.herokuapp.com/pointtable').then(res => {
-        setTable(res.data)
+        const response = await axios.get('https://enfuego.herokuapp.com/pointtable')
+        setTable(response.data)
         setLoading(false)
-    }).catch(err => console.log(err))
-
-    const clickHandler = (name) => {
-        var selectedTeam = name
-
-        axios.get(`https://enfuego.herokuapp.com/specificfixtures/?team=${selectedTeam}`).then(res => {
-            console.log(res.data)
-            setTeam(res.data)
-
-        }).catch(err => console.log(err))
-    }
-
-    const fixtureSpecific = (data) => {
-        return (
-            <div className="">
-                {data.map((datas,key) => (
-                    <Fixturecard data={datas} />
-                ))}
-            </div>
-        )
+        console.log(response.data);
     }
     useEffect(() => {
-        fixtureSpecific(team)
-    }, [team])
+        fetchTable()
+    }, [])
+    // axios.get('https://enfuego.herokuapp.com/pointtable').then(res => {
+    //     setTable(res.data)
+    //     setLoading(false)
+    //     console.log(res.data)
+    // }).catch(err => console.log(err))
+
+    // const clickHandler = (name) => {
+    //     var selectedTeam = name
+
+    //     axios.get(`https://enfuego.herokuapp.com/specificfixtures/?team=${selectedTeam}`).then(res => {
+    //         console.log(res.data)
+    //         setTeam(res.data)
+
+    //     }).catch(err => console.log(err))
+    // }
+
+    // const fixtureSpecific = (data) => {
+    //     return (
+    //         <div className="">
+    //             {data.map((datas,key) => (
+    //                 <Fixturecard data={datas} />
+    //             ))}
+    //         </div>
+    //     )
+    // }
+    // useEffect(() => {
+    //     fixtureSpecific(team)
+    // }, [team])
     return (
         <div className="md:flex items-center justify-center mx-8">
             {loading ? <>
@@ -54,11 +57,12 @@ const Pointtable = () => {
                 <div className=''>
                 <h3 className='text-center text-2xl font-bold mb-6'>League Standings</h3>
                     <div className='grid grid-cols-8 mb-8  md:text-xl font-bold xs:w-screen md:w-[50rem] border-b-2 border-black pb-4'>
-                        <div className="col-span-3 md:pl-16 ">
+                        <div className="col-span-2 md:pl-16 ">
                             Team
                         </div>
                         <div className=" col-span-1">M</div>
                         <div className=" col-span-1">W</div>
+                        <div className=" col-span-1">L</div>
                         <div className=" col-span-1">GD</div>
                         <div className=" col-span-1">GS</div>
                         <div className=" col-span-1">Pts</div>
@@ -66,11 +70,13 @@ const Pointtable = () => {
                     </div>
                     {table?.map((data) => (
                         <div className='grid grid-cols-8 mt-4 md:w-[50rem] border-b-2 pb-4'>
-                            <div onClick={() => clickHandler(data.team_id)} className="col-span-3 md:text-xl font-semibold md:pl-12">
+                            <div  className="col-span-2 md:text-xl font-semibold md:pl-12">
                                 {data.team_id}
                             </div>
                             <div className=" col-span-1 ">{data.gamesplayed}</div>
+                            
                             <div className=" col-span-1">{data.gameswon}</div>
+                            <div className=" col-span-1 ">{data.gameslost}</div>
                             <div className=" col-span-1">{data.goaldifference}</div>
                             <div className=" col-span-1">{data.goalsscored}</div>
                             <div className=" col-span-1">{data.points}</div>
