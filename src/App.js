3498cf9d6d7296b0ec75145aef6362fd36c80d08
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-06-25 12:30:47 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import './index.css'
 import Pointtable from './components/Pointtable';
 import Fixtures from './components/Fixtures';
 import Goldenboot from './components/Goldenboot';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App overflow-y-scroll">
-      {/* <Fixtures /> */}
-      {/* <Pointtable /> */}
-      <Goldenboot />
+    <Navbar />
+    <div className="mt-24">
+    <Routes >
+      <Route path='/' element={<Pointtable/>} />
+      <Route path='/fixtures' element={<Fixtures/>} />
+      <Route path='/goldenboot' element={<Goldenboot/>} />
+
+    </Routes>
+
+     
+    </div>
+     
     </div>
   );
 }
